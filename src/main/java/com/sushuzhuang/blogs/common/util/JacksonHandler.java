ff1b2d305243834680b8e2e5bbf089a1d4e88046
shuzhuang.su <shuzhuang.su@11bee.com>
2020-04-29 16:41:58 +0800

    brisn

---
+++ src/main/java/com/sushuzhuang/blogs/common/util/JacksonHandler.java
@@ -0,0 +1,90 @@
+package com.sushuzhuang.blogs.common.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ */
+public final class JacksonHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(JacksonHandler.class);
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    static {
+        mapper.disable(SerializationFeature.INDENT_OUTPUT);
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mapper.disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES);
+
+        mapper.configure(JsonParser.Feature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER, true);
+        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+        mapper.configure(JsonParser.Feature.ALLOW_NON_NUMERIC_NUMBERS, true);
+        mapper.configure(JsonParser.Feature.ALLOW_NUMERIC_LEADING_ZEROS, true);
+        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+    }
+
+    private JacksonHandler() {
+    }
+
+    public static String toSerialize(Object data) {
+        try {
+            return mapper.writeValueAsString(data);
+        } catch (Exception e) {
+            logger.error("toSerialize error", e);
+            return null;
+        }
+    }
+
+    public static <T> T deSerialize(String content, Class<T> clazz) {
+        try {
+            return mapper.readValue(content, clazz);
+        } catch (IOException e) {
+            logger.error("deSerialize error: {}", content, e);
+            return null;
+        }
+    }
+
+    public static <T> Map<String, T> deSerializeMap(Class<T> clazeValue, String content) {
+        try {
+            JavaType javaType = mapper.getTypeFactory().constructParametrizedType(Map.class, Map.class, String.class, clazeValue);
+            return mapper.readValue(content, javaType);
+        } catch (IOException e) {
+            logger.error("deserialize with java type object error: {}", content, e);
+            return null;
+        }
+    }
+
+    public static <T> List<T> deSerializeList(String json, Class<T> elementClasse) {
+        List<T> t = new ArrayList<>();
+        try {
+            JavaType javaType = mapper.getTypeFactory().constructParametrizedType(List.class, List.class, elementClasse);
+            t = mapper.readValue(json, javaType);
+        } catch (IOException e) {
+            logger.error("deserialize with java type object error: {}", json, e);
+        }
+        return t;
+    }
+
+
+    public static <T> T parseJson(String content, Class<T> typeReference) {
+        try {
+            return mapper.readValue(content, typeReference);
+        } catch (IOException var3) {
+            logger.error("deserialize TypeReference error: {}", content, var3);
+            throw new RuntimeException("parseJson error", var3);
+        }
+    }
+
+}
--- src/main/java/com/sushuzhuang/blogs/controller/IndexController.java
+++ src/main/java/com/sushuzhuang/blogs/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.sushuzhuang.blogs.controller;
 
+import com.sushuzhuang.blogs.common.util.JacksonHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 初始化
@@ -16,5 +19,14 @@ public class IndexController {
     @RequestMapping("")
     public String init(){
         return "index";
+    }
+
+
+    @RequestMapping("/get")
+    public void test(HttpServletRequest request){
+
+        System.out.println(request.getRequestURI());
+
+        System.out.println(JacksonHandler.toSerialize(request.getParameterMap()));
     }
 }
